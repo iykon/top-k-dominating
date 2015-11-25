c62e30a681d14d5e553e3aeeee9b4528a48a5607
iykon <447465090@qq.com>
2015-11-25 15:44:56 -0800

    supporte two dominating types: larger and smaller

--- tkd.sql
+++ tkd.sql
@@ -1,3 +1,3 @@
-CREATE OR REPLACE FUNCTION tkd(text, int) RETURNS SETOF record
+CREATE OR REPLACE FUNCTION tkd(text, int, int) RETURNS SETOF record
 	AS '$libdir/tkd', 'tkd_query'
 	LANGUAGE C STRICT;
